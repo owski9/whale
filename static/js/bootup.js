939f6df5bc74d14271394400754b3219d7f10bb6
Lunarion <117928931+Lunarion31@users.noreply.github.com>
2024-05-07 01:30:00 -0400

    Update bootup.js

--- static/js/bootup.js
+++ static/js/bootup.js
@@ -36,13 +36,7 @@ function showBootUpSequence() {
   }
 
   function showAlertAndPlayMusic() {
-     var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-  if (!isChrome){
-      $('#iframeAudio').remove()
-  }
-  else {
-      $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
-  }
+     var context = new AudioContext();
   }
   
   const bootUpInterval = setInterval(() => {
