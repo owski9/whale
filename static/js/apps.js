51a2e272724d18507a1e6a9b8b1717c1a4655db0
Lunarion <117928931+Lunarion31@users.noreply.github.com>
2024-05-08 17:15:40 -0400

    Update apps.js

--- static/js/apps.js
+++ static/js/apps.js
@@ -110,7 +110,7 @@
       </div>
       </div>
         <div class="about-content" style="display: flex; justify-content: center; align-itmes: flex-start; margin-top: 40px;">
-            <img src="static/img/0950ou.png" alt="About Image" draggable="false" style="max-width: 100%; height: auto; border: 2px outset #c8c7c7;">
+            <img src="static/img/0950ou.png" alt="About Image" draggable="false" style="max-width: 50%; height: 50%; border: 2px outset #c8c7c7;">
             <div class="about-text" style="border: 2px inset #fff;">
                 <p>Hi there! I'm Lunarion. I'm a 16-year-old high-schooler who has a passion for programming. Feel free to contact me!</p>
                 <button class="contact-btn" onclick="openContactWindow()" style="border: 2px outset #c8c7c7;">Contact</button>
