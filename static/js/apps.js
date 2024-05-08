2099c01a8bc03e3df75c90ef6016f64f1d957135
Lunarion <117928931+Lunarion31@users.noreply.github.com>
2024-05-08 12:46:20 -0400

    Update apps.js

--- static/js/apps.js
+++ static/js/apps.js
@@ -199,7 +199,7 @@
                     <img src="static/svg/lyt65r.svg" alt="Close" class="window-button" onclick="closeWindow(this)">
                 </div>
             </div>
-            <div class="calendar">
+            <div class="calendar" style="border: 2px outset #c0c0c0;">
                 <div class="calendar-day">Sun<span class="current-day-dot"></span></div>
                 <div class="calendar-day">Mon</div>
                 <div class="calendar-day">Tue</div>
